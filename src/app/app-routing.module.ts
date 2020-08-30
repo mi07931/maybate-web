7538246fa9540d733c3666889132410d31e6dfbb
mi07931 <mi07931@gmail.com>
2020-08-30 22:31:49 +0100

    merge with DW

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+import { GridComponent } from './grid/grid.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: '', component: HomeComponent },
+    { path: 'cards', component: CardsComponent },
+    { path: 'grid', component: GridComponent },
+    { path: 'contact', component: ContactComponent }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardsComponent } from './cards/cards.component';
+import { GridComponent } from './grid/grid.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardsComponent,
+    GridComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
